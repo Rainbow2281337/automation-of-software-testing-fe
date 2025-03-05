072819f63e7a958749b19b2ee02d15a64b697c49
Oleksii <alexey.shevchenko.03@gmail.com>
2025-03-05 08:23:36 +0200

    FE-2: Add registration functionality with form validation and routing

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registration-page',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './registration-page.component.html',
-  styleUrl: './registration-page.component.scss'
+  styleUrl: './registration-page.component.scss',
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit {
+  /**
+   * The registration data of the {@link FormGroup}
+   */
+  public regForm!: FormGroup;
 
+  /**
+   * Indicates if the register button was clicked
+   *
+   *  @memberof AuthPageComponent
+   */
+  public submitted: boolean = false;
+
+  constructor(private readonly authSerice: AuthService) {}
+
+  ngOnInit(): void {
+    this.regForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      userName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+    });
+  }
+
+  emitRegistration(event: Event) {
+    event.preventDefault();
+    this.submitted = true;
+
+    if (this.regForm.invalid) {
+      return;
+    }
+    const { email, userName, password } = this.regForm.value;
+
+    this.authSerice.register(email, userName, password).subscribe({
+      next: (response) => {
+        console.log(response); // TODO: REMOVE THIS LINE
+      },
+      error: (error) => {
+        console.error('Error:', error); // TODO: REPALCE WITH ERROR HANDLER
+      },
+    });
+  }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,20 +1,59 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, tap } from 'rxjs';
-import { UserPayload } from '../../auth-page/share/interface';
+import { UserPayload } from '../../auth-page/shared/interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000/auth';
+  private readonly apiUrl = 'http://localhost:5000/auth';
 
   constructor(private readonly http: HttpClient) {}
 
+  /**
+   * Takes the user login data and sends it to the server.
+   * In response, if the user found, the server sends a token
+   * that is saved in the local storage.
+   *
+   * @param email - User email
+   * @param password - User password
+   * @returns - Observable<UserPayload>
+   */
   login(email: string, password: string): Observable<UserPayload> {
     const payload = { email, password };
 
     return this.http.post<UserPayload>(`${this.apiUrl}/login`, payload).pipe(
+      tap((response) => {
+        if (response.access_token) {
+          localStorage.setItem('auth_token', response.access_token);
+        }
+      }),
+      catchError((error) => {
+        console.error('Error:', error);
+        throw error;
+      })
+    );
+  }
+
+  /**
+   * Takes the user registration data and sends it to the server.
+   * In response, if the user found, server sends a token
+   * that is saved in the local storage.
+   *
+   * @param email - User email
+   * @param userName - User userName
+   * @param password - User password
+   * @returns - Observable<UserPayload>
+   */
+  register(
+    email: string,
+    userName: string,
+    password: string
+  ): Observable<UserPayload> {
+    const payload = { email, userName, password };
+
+    return this.http.post<UserPayload>(`${this.apiUrl}/register`, payload).pipe(
       tap((response) => {
         if (response.access_token) {
           localStorage.setItem('auth_token', response.access_token);
